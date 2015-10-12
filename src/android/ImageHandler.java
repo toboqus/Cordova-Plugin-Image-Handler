91a329078c111fd1e7c274dab5faf26309e60103
Alex Yorke <me@alexyorke.net>
2015-10-12 14:13:49 +0100

    possibly finish base64ToJpg

--- src/android/ImageHandler.java
+++ src/android/ImageHandler.java
@@ -1,11 +1,19 @@
 package net.alexyorke.imagehandler;
 
-import android.util.Log;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 
 public class ImageHandler extends CordovaPlugin {
@@ -48,16 +56,60 @@ public class ImageHandler extends CordovaPlugin {
     }
 
 
+    /**
+     *
+     */
     private void base64ToJpg(CallbackContext callbackContext, JSONArray args){
 
+        String base64Image;
+        String directory;
+        String imageName;
+        String mFilePath;
+
         try {
-            for (int i = 0; i < args.length(); i++) {
-                Log.d(LOG_TAG, args.getString(i));
-            }
+            base64Image = args.getString(0);
+            directory = args.getString(1);
+            imageName = args.getString(2);
         }catch(JSONException e){
             e.printStackTrace();
+            callbackContext.error("Could not parse the parameters");
+            return;
         }
-        callbackContext.success(); // Thread-safe.
+
+        //does not contain predata - reject
+        if(base64Image.length() < 22){
+            callbackContext.error("base64 string is not formatted correctly");
+            return;
+        }
+
+        try{
+            String imageDataBytes = base64Image.substring(base64Image.indexOf(",") + 1);
+            InputStream is = new ByteArrayInputStream(Base64.decode(imageDataBytes, Base64.DEFAULT));
+            Bitmap image= BitmapFactory.decodeStream(is);
+
+            if (!new File(directory).exists()) {
+                callbackContext.error("Directory: "+directory+" does not exist!");
+                return;
+            }
+
+            mFilePath = directory + "/" + imageName + ".jpg";
+            File file = new File(mFilePath);
+            FileOutputStream stream = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+            is.close();
+            image.recycle();
+            stream.flush();
+            stream.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            callbackContext.error("Could not save image");
+            return;
+        }
+
+        callbackContext.success(mFilePath); // Thread-safe.
     }
 
     private void resize(CallbackContext callbackContext, JSONArray args){
