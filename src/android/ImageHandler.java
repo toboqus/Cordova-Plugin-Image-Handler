baf4eb8f925f3f625d1ecf0586972a8384ad55e2
Alex Yorke <me@alexyorke.net>
2015-10-13 10:51:53 +0100

    base64ToJpg now working, layering bitmaps

--- src/android/ImageHandler.java
+++ src/android/ImageHandler.java
@@ -3,6 +3,9 @@ package net.alexyorke.imagehandler;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.util.Base64;
 
 import org.apache.cordova.CallbackContext;
@@ -10,10 +13,10 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.IOException;
+import java.net.URI;
 
 
 public class ImageHandler extends CordovaPlugin {
@@ -57,7 +60,10 @@ public class ImageHandler extends CordovaPlugin {
 
 
     /**
-     *
+     * This method will attempt to save a base64 string to a jpg file with the
+     * given directory and imageName
+     * @param callbackContext callback to respond with
+     * @param args arguments in this case are: base64Image, directory, imageName
      */
     private void base64ToJpg(CallbackContext callbackContext, JSONArray args){
 
@@ -67,9 +73,16 @@ public class ImageHandler extends CordovaPlugin {
         String mFilePath;
 
         try {
+
             base64Image = args.getString(0);
             directory = args.getString(1);
             imageName = args.getString(2);
+            if(directory == null || imageName == null){
+                callbackContext.error("Could not parse the parameters");
+                return;
+            }
+            mFilePath = directory + imageName + ".jpg";
+
         }catch(JSONException e){
             e.printStackTrace();
             callbackContext.error("Could not parse the parameters");
@@ -77,33 +90,46 @@ public class ImageHandler extends CordovaPlugin {
         }
 
         //does not contain predata - reject
-        if(base64Image.length() < 22){
-            callbackContext.error("base64 string is not formatted correctly");
+        if(!isValidImage(base64Image) || !isValidExtension(getBase64Extension(base64Image))){
+            callbackContext.error("base64 string is not a valid image");
             return;
         }
 
         try{
             String imageDataBytes = base64Image.substring(base64Image.indexOf(",") + 1);
-            InputStream is = new ByteArrayInputStream(Base64.decode(imageDataBytes, Base64.DEFAULT));
-            Bitmap image= BitmapFactory.decodeStream(is);
+            Bitmap finalImage = whiteBackground(Base64ToBitmap(imageDataBytes));
 
-            if (!new File(directory).exists()) {
-                callbackContext.error("Directory: "+directory+" does not exist!");
-                return;
+            File directoryLoc = new File(URI.create(directory));
+            File file = new File(URI.create(mFilePath));
+
+            //ensure that directory structure exists
+            if (!directoryLoc.exists()) {
+                directoryLoc.mkdirs();
+            }
+            //create file if it doesnt exist
+            if (!file.exists()){
+                file.createNewFile();
             }
 
-            mFilePath = directory + "/" + imageName + ".jpg";
-            File file = new File(mFilePath);
-            FileOutputStream stream = new FileOutputStream(file);
-            image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            FileOutputStream out = null;
+            try {
+                out = new FileOutputStream(file);
+                finalImage.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            is.close();
-            image.recycle();
-            stream.flush();
-            stream.close();
-        }
-        catch(Exception e)
-        {
+                finalImage.recycle();
+            }
+
+        } catch(Exception e) {
             e.printStackTrace();
             callbackContext.error("Could not save image");
             return;
@@ -122,6 +148,111 @@ public class ImageHandler extends CordovaPlugin {
 
     private void rotate(CallbackContext callbackContext, JSONArray args){
         callbackContext.success(); // Thread-safe.
+    }
+
+
+    //-------------------HELPER METHODS---------------------------
+
+    /**
+     *
+     * @param base64Image base64 encoded string of the image
+     * @return true if the image is valid, false if not
+     */
+    private boolean isValidImage(String base64Image){
+
+        if(base64Image == null){
+            return false;
+        }
+
+        String crop1 = base64Image.substring(base64Image.indexOf(":") + 1);
+        String type = crop1.substring(0,crop1.indexOf("/"));
+
+        return (type.equals("image"));
+
+    }
+
+    /**
+     *
+     * @param extension extension of the image
+     * @return true if the extension is supported, or false if not
+     */
+    private boolean isValidExtension(String extension){
+
+        String[] validExtensions = {"jpg", "jpeg", "png"};
+
+        for(String ext : validExtensions){
+            if(ext.equals(extension)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * will return the extension of the image
+     * @param base64Image the base64 encoded image
+     * @return the extension of the image
+     */
+    private String getBase64Extension(String base64Image){
+
+        String imageDataBytes = base64Image.substring(base64Image.indexOf("/") + 1);
+        String ext = imageDataBytes.substring(0,imageDataBytes.indexOf(";"));
+
+        return (isValidExtension(ext)) ? ext : null;
+    }
+
+    /**
+     * will attempt to get a Bitmap from a base64 string
+     * @param myImageData base64 string to turn into a bitmap image
+     * @return Bitmap
+     */
+    private Bitmap Base64ToBitmap(String myImageData){
+        byte[] imageAsBytes = Base64.decode(myImageData.getBytes(), Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
+    }
+
+
+    /**
+     * This method will overlay two bitmap images on top
+     * of each other.
+     *
+     * @param bmp1 background image
+     * @param bmp2 layer image
+     * @return Bitmap
+     */
+    private Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
+        if(bmp1 == null || bmp2 == null){
+            return null;
+        }
+
+        Bitmap bmOverlay = Bitmap.createBitmap(bmp1.getWidth(), bmp1.getHeight(), bmp1.getConfig());
+        Canvas canvas = new Canvas(bmOverlay);
+        canvas.drawBitmap(bmp1, new Matrix(), null);
+        canvas.drawBitmap(bmp2, new Matrix(), null);
+        return bmOverlay;
+    }
+
+    /**
+     * This method will ensure that transparency is handled
+     * when saving pngs to a jpg
+     *
+     * @param bmp1 image that needs to have a white background
+     * @return Bitmap
+     */
+    private Bitmap whiteBackground(Bitmap bmp1){
+        if(bmp1 == null){
+            return null;
+        }
+
+        Bitmap background = Bitmap.createBitmap(bmp1.getWidth(), bmp1.getHeight(), bmp1.getConfig());
+        background.eraseColor(Color.WHITE);
+        Bitmap finalImage = overlay(background, bmp1);
+
+        background.recycle();
+        bmp1.recycle();
+
+        return finalImage;
     }
 
 }
