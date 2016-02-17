bfa9872cbbf8de5d82cdc33c5e4162b4ebe8dae3
Roman Kisil <romankisil@users.noreply.github.com>
2016-02-17 08:58:05 +0000

    Updated to include the timestamp function

--- src/ios/ImageHandler.h
+++ src/ios/ImageHandler.h
@@ -6,6 +6,7 @@
 - (void) resize:(CDVInvokedUrlCommand*)command;
 - (void) thumbnail:(CDVInvokedUrlCommand*)command;
 - (void) rotate:(CDVInvokedUrlCommand*)command;
+- (void) timestamp:(CDVInvokedUrlCommand*)command;
 
 
 @end
